c856eb20f52f16a1def1cd2f09c3e2ae4dc7a4fd
Aren Chen <yarencheng@gmail.com>
2017-02-11 08:42:37 +0800

    first commit

---
+++ src/main/java/arenx/btrobot/Main.java
@@ -0,0 +1,14 @@
+package arenx.btrobot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] argv) {
+		// TODO Auto-generated method stub
+		logger.info("hellp");
+	}
+}
